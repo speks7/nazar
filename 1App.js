5f9b074165ebabdc138ba5f3ea070e4a9e10e641
Sunim Acharya <sunim.54@gmail.com>
2018-02-28 14:42:35 +0545

    Update react-navigation package

--- 1App.js
+++ 1App.js
@@ -12,11 +12,14 @@ import {
   View
 } from 'react-native';
 
-import { StackNavigator } from 'react-navigation'
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
 
-import HomeScreen from './screens/HomeScreen'
-import PredictScreen from './screens/PredictScreen'
-
+type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
