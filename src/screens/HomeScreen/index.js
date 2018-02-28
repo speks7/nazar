930d7057212a59da29aca64531e6fd7fbbe06f95
Sunim Acharya <sunim.54@gmail.com>
2018-02-28 15:06:19 +0545

    Add components for calling in Home

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -1,29 +1,49 @@
-import React, { Component } from 'react'
-import { Alert, View, StatusBar, ActivityIndicator } from 'react-native'
-import PropTypes from 'prop-types'
+import React, { Component } from "react";
+import { Alert, View, StatusBar, ActivityIndicator } from "react-native";
+import PropTypes from "prop-types";
 
-import styles from './styles'
+import Header from '../../components/Header'
+import BackgroundImage from '../../components/BackgroundImage'
+import XPButton from '../../components/XPButton'
+
+import styles from "./styles";
 
 class HomeScreen extends Component {
-    static navigationOptions = {
-      header: <Header title='nazar' subtitle='"Electronic component detection system"' />,
-    }
-    
-    constructor() {
-      super()
-  
-      this.state = {
-        loading: false,
-      }
-  
-    render() {
+  static navigationOptions = {
+    header: (
+      <Header
+        title="nazar"
+        subtitle="&quot;Electronic component detection system&quot;"
+      />
+    )
+  };
 
-    }
+  constructor() {
+    super();
+
+    this.state = {
+      loading: false
+    };
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <StatusBar hidden />
+        <BackgroundImage source={require("../../assets/bkg.jpg")}>
+          {!this.state.loading ? (
+            <XPButton title="Analyze an image" onPress={this._onClick} />
+          ) : (
+            <ActivityIndicator size="large" color="#e74c3c" />
+          )}
+        </BackgroundImage>
+      </View>
+    );
   }
 }
-  
-  HomeScreen.propTypes = {
-    navigation: PropTypes.object,
-  }
-  
-  export default HomeScreen
+
+HomeScreen.propTypes = {
+  navigation: PropTypes.object
+};
+
+export default HomeScreen;
