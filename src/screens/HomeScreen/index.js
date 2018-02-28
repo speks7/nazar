2b6b660eddc2fca757ebd3bf38c97456911e1277
Sunim Acharya <sunim.54@gmail.com>
2018-02-28 14:49:25 +0545

    Setup basic layout

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -1,3 +1,29 @@
 import React, { Component } from 'react'
 import { Alert, View, StatusBar, ActivityIndicator } from 'react-native'
 import PropTypes from 'prop-types'
+
+import styles from './styles'
+
+class HomeScreen extends Component {
+    static navigationOptions = {
+      header: <Header title='nazar' subtitle='"Electronic component detection system"' />,
+    }
+    
+    constructor() {
+      super()
+  
+      this.state = {
+        loading: false,
+      }
+  
+    render() {
+
+    }
+  }
+}
+  
+  HomeScreen.propTypes = {
+    navigation: PropTypes.object,
+  }
+  
+  export default HomeScreen
--- src/screens/PredictScreen/index.js
+++ src/screens/PredictScreen/index.js
@@ -1,3 +1,36 @@
 import React, { Component } from 'react'
 import { ActivityIndicator, View, Text, StatusBar, Alert, } from 'react-native'
 import PropTypes from 'prop-types'
+
+import styles from './styles'
+
+class PredictScreen extends Component {
+    static navigationOptions = {
+      header: null,
+    }
+  
+    constructor(props) {
+      super(props)
+  
+      this.state = {
+        loading: true,
+        result: '',
+      }
+    }
+  
+    componentDidMount() {
+      })
+  
+  
+    render() {
+
+      return (
+      )
+    }
+  }
+  
+  PredictScreen.propTypes = {
+    navigation: PropTypes.object,
+  }
+  
+  export default PredictScreen
