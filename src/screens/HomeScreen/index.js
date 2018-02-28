e29a4324d5c5dcaa8058f64a22b45a765da6ee18
Sunim Acharya <sunim.54@gmail.com>
2018-02-28 15:10:23 +0545

    Setup for onclick function

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -2,9 +2,9 @@ import React, { Component } from "react";
 import { Alert, View, StatusBar, ActivityIndicator } from "react-native";
 import PropTypes from "prop-types";
 
-import Header from '../../components/Header'
-import BackgroundImage from '../../components/BackgroundImage'
-import XPButton from '../../components/XPButton'
+import Header from "../../components/Header";
+import BackgroundImage from "../../components/BackgroundImage";
+import XPButton from "../../components/XPButton";
 
 import styles from "./styles";
 
@@ -24,6 +24,28 @@ class HomeScreen extends Component {
     this.state = {
       loading: false
     };
+
+    this._onClick = this._onClick.bind(this);
+  }
+
+  _onClick() {
+    this.setState({ loading: true });
+    ImagePicker.showImagePicker(this.options, response => {
+      if (response.didCancel) {
+        this.setState({ loading: false });
+      } else if (response.error) {
+        Alert.alert(
+          "Error",
+          "Check the permissions.",
+          { cancelable: false }
+        );
+        this.setState({ loading: false });
+      } else {
+        const { navigate } = this.props.navigation;
+        navigate("Prediction", { image: response });
+        this.setState({ loading: false });
+      }
+    });
   }
 
   render() {
