f168c563e0313aceacab5baab1831d4e428590c3
Sunim Acharya <sunim.54@gmail.com>
2018-02-28 14:38:31 +0545

    Add Directory index

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import { AppRegistry } from 'react-native'
 import { StackNavigator } from 'react-navigation'
 
-import HomeScreen from './screens/HomeScreen'
-import PredictScreen from './screens/PredictScreen'
+import HomeScreen from './src/screens/HomeScreen'
+import PredictScreen from './src/screens/PredictScreen'
 
 const App = StackNavigator({
   Home: { screen: HomeScreen },
