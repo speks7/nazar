ac97157b6bc363f53cd2abf4e75f7361c5c79f9b
Sunim Acharya <sunim.54@gmail.com>
2018-03-03 10:24:57 +0545

    Add vector-icons

--- android/app/src/main/java/com/nazar/MainApplication.java
+++ android/app/src/main/java/com/nazar/MainApplication.java
@@ -3,6 +3,7 @@ package com.nazar;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import fr.greweb.reactnativeviewshot.RNViewShotPackage;
 import cl.json.RNSharePackage;
 import com.imagepicker.ImagePickerPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
             new RNViewShotPackage(),
             new RNSharePackage(),
             new ImagePickerPackage(),
