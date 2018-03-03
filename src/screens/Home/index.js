55f58f28e71693e69e7fe2e2210c92988b6a3b02
Sunim Acharya <sunim.54@gmail.com>
2018-03-03 13:32:49 +0545

    Import elements view

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -5,8 +5,14 @@ import {
   StatusBar,
   ActivityIndicator,
   UIManager,
-  ImageBackground
+  ImageBackground,
+  KeyboardAvoidingView,
+  Text,
+  LayoutAnimation
 } from "react-native";
+
+import { Input, Button } from "react-native-elements";
+
 import PropTypes from "prop-types";
 
 import ImagePicker from "react-native-image-picker";
@@ -21,6 +27,18 @@ const BG_IMAGE = require("../../assets/bkg.jpg");
 
 UIManager.setLayoutAnimationEnabledExperimental &&
   UIManager.setLayoutAnimationEnabledExperimental(true);
+
+const TabSelector = ({ selected }) => {
+  return (
+    <View style={styles.selectorContainer}>
+      <View style={selected && styles.selected} />
+    </View>
+  );
+};
+
+TabSelector.propTypes = {
+  selected: PropTypes.bool.isRequired
+};
 
 class Home extends Component {
   /*
@@ -40,7 +58,21 @@ class Home extends Component {
       loading: false
     };
 
+    this.state = {
+      email: "",
+      password: "",
+      fontLoaded: false,
+      selectedCategory: 0,
+      isLoading: false,
+      isEmailValid: true,
+      isPasswordValid: true,
+      isConfirmationValid: true
+    };
+
     this._onClick = this._onClick.bind(this);
+    this.selectCategory = this.selectCategory.bind(this);
+    this.login = this.login.bind(this);
+    this.signUp = this.signUp.bind(this);
 
     this.options = {
       title: "Select an image",
@@ -72,16 +104,196 @@ class Home extends Component {
     });
   }
 
+  selectCategory(selectedCategory) {
+    LayoutAnimation.easeInEaseOut();
+    this.setState({
+      selectedCategory,
+      isLoading: false
+    });
+  }
+
+  validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    return re.test(email);
+  }
+
+  login() {
+    const { email, password } = this.state;
+    this.setState({ isLoading: true });
+    // Simulate an API call
+    setTimeout(() => {
+      LayoutAnimation.easeInEaseOut();
+      this.setState({
+        isLoading: false,
+        isEmailValid: this.validateEmail(email) || this.emailInput.shake(),
+        isPasswordValid: password.length >= 8 || this.passwordInput.shake()
+      });
+    }, 1500);
+  }
+
+  signUp() {
+    const { email, password, passwordConfirmation } = this.state;
+    this.setState({ isLoading: true });
+    // Simulate an API call
+    setTimeout(() => {
+      LayoutAnimation.easeInEaseOut();
+      this.setState({
+        isLoading: false,
+        isEmailValid: this.validateEmail(email) || this.emailInput.shake(),
+        isPasswordValid: password.length >= 8 || this.passwordInput.shake(),
+        isConfirmationValid:
+          password == passwordConfirmation || this.confirmationInput.shake()
+      });
+    }, 1500);
+  }
+
   render() {
+    const {
+      selectedCategory,
+      isLoading,
+      isEmailValid,
+      isPasswordValid,
+      isConfirmationValid,
+      email,
+      password,
+      passwordConfirmation
+    } = this.state;
+    const isLoginPage = selectedCategory === 0;
+    const isSignUpPage = selectedCategory === 1;
     return (
       <View style={styles.container}>
-        <StatusBar hidden />
         <ImageBackground source={BG_IMAGE} style={styles.bgImage}>
-          {!this.state.loading ? (
-            <XPButton title="Analyze an image" onPress={this._onClick} />
-          ) : (
-            <ActivityIndicator size="large" color="#e74c3c" />
-          )}
+          <View>
+            <KeyboardAvoidingView
+              contentContainerStyle={styles.loginContainer}
+              behavior="position"
+            >
+              <View style={styles.titleContainer}>
+                <View style={{ flexDirection: "row" }}>
+                  <Text style={styles.titleText}>BEAUX</Text>
+                </View>
+                <View style={{ marginTop: -10, marginLeft: 10 }}>
+                  <Text style={styles.titleText}>VOYAGES</Text>
+                </View>
+              </View>
+              <View style={{ flexDirection: "row" }}>
+                <Button
+                  disabled={isLoading}
+                  clear
+                  activeOpacity={0.7}
+                  onPress={() => this.selectCategory(0)}
+                  textStyle={[
+                    styles.categoryText,
+                    isLoginPage && styles.selectedCategoryText
+                  ]}
+                  text={"Login"}
+                />
+                <Button
+                  disabled={isLoading}
+                  clear
+                  activeOpacity={0.7}
+                  onPress={() => this.selectCategory(1)}
+                  textStyle={[
+                    styles.categoryText,
+                    isSignUpPage && styles.selectedCategoryText
+                  ]}
+                  text={"Sign up"}
+                />
+              </View>
+              <View style={styles.rowSelector}>
+                <TabSelector selected={isLoginPage} />
+                <TabSelector selected={isSignUpPage} />
+              </View>
+              <View style={styles.formContainer}>
+                <Input
+                  value={email}
+                  keyboardAppearance="light"
+                  autoFocus={false}
+                  autoCapitalize="none"
+                  autoCorrect={false}
+                  keyboardType="email-address"
+                  returnKeyType="next"
+                  inputStyle={{ marginLeft: 10 }}
+                  placeholder={"Email"}
+                  containerStyle={{
+                    borderBottomColor: "rgba(0, 0, 0, 0.38)"
+                  }}
+                  ref={input => (this.emailInput = input)}
+                  onSubmitEditing={() => this.passwordInput.focus()}
+                  onChangeText={email => this.setState({ email })}
+                  displayError={!isEmailValid}
+                  errorMessage="Please enter a valid email address"
+                />
+                <Input
+                  value={password}
+                  keyboardAppearance="light"
+                  autoCapitalize="none"
+                  autoCorrect={false}
+                  secureTextEntry={true}
+                  returnKeyType={isSignUpPage ? "next" : "done"}
+                  blurOnSubmit={true}
+                  containerStyle={{
+                    marginTop: 16,
+                    borderBottomColor: "rgba(0, 0, 0, 0.38)"
+                  }}
+                  inputStyle={{ marginLeft: 10 }}
+                  placeholder={"Password"}
+                  ref={input => (this.passwordInput = input)}
+                  onSubmitEditing={() =>
+                    isSignUpPage ? this.confirmationInput.focus() : this.login()
+                  }
+                  onChangeText={password => this.setState({ password })}
+                  displayError={!isPasswordValid}
+                  errorMessage="Please enter at least 8 characters"
+                />
+                {isSignUpPage && (
+                  <Input
+                    value={passwordConfirmation}
+                    secureTextEntry={true}
+                    keyboardAppearance="light"
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                    keyboardType="default"
+                    returnKeyType={"done"}
+                    blurOnSubmit={true}
+                    containerStyle={{
+                      marginTop: 16,
+                      borderBottomColor: "rgba(0, 0, 0, 0.38)"
+                    }}
+                    inputStyle={{ marginLeft: 10 }}
+                    placeholder={"Confirm password"}
+                    ref={input => (this.confirmationInput = input)}
+                    onSubmitEditing={this.signUp}
+                    onChangeText={passwordConfirmation =>
+                      this.setState({ passwordConfirmation })
+                    }
+                    displayError={!isConfirmationValid}
+                    errorMessage="Please enter the same password"
+                  />
+                )}
+                <Button
+                  buttonStyle={styles.loginButton}
+                  containerStyle={{ marginTop: 32, flex: 0 }}
+                  activeOpacity={0.8}
+                  text={isLoginPage ? "LOGIN" : "SIGN UP"}
+                  onPress={isLoginPage ? this.login : this.signUp}
+                  textStyle={styles.loginTextButton}
+                  loading={isLoading}
+                  disabled={isLoading}
+                />
+              </View>
+            </KeyboardAvoidingView>
+            <View style={styles.helpContainer}>
+              <Button
+                text={"Need help ?"}
+                textStyle={{ color: "white" }}
+                buttonStyle={{ backgroundColor: "transparent" }}
+                underlayColor="transparent"
+                onPress={() => console.log("Account created")}
+              />
+            </View>
+          </View>
         </ImageBackground>
       </View>
     );
--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -5,9 +5,60 @@ const SCREEN_HEIGHT = Dimensions.get("window").height;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: "center",
+    flex: 1
+  },
+  rowSelector: {
+    height: 20,
+    flexDirection: "row",
     alignItems: "center"
+  },
+  selectorContainer: {
+    flex: 1,
+    alignItems: "center"
+  },
+  selected: {
+    position: "absolute",
+    borderRadius: 50,
+    height: 0,
+    width: 0,
+    top: -5,
+    borderRightWidth: 70,
+    borderBottomWidth: 70,
+    borderColor: "white",
+    backgroundColor: "white"
+  },
+  loginContainer: {
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  loginTextButton: {
+    fontSize: 16,
+    color: "white",
+    fontWeight: "bold"
+  },
+  loginButton: {
+    backgroundColor: "rgba(232, 147, 142, 1)",
+    borderRadius: 10,
+    height: 50,
+    width: 200
+  },
+  titleContainer: {
+    height: 150,
+    backgroundColor: "transparent",
+    justifyContent: "center"
+  },
+  formContainer: {
+    backgroundColor: "white",
+    width: SCREEN_WIDTH - 30,
+    borderRadius: 10,
+    paddingTop: 32,
+    paddingBottom: 32,
+    alignItems: "center"
+  },
+  loginText: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "white"
   },
   bgImage: {
     flex: 1,
@@ -17,6 +68,27 @@ const styles = StyleSheet.create({
     height: SCREEN_HEIGHT,
     justifyContent: "center",
     alignItems: "center"
+  },
+  categoryText: {
+    textAlign: "center",
+    color: "white",
+    fontSize: 24,
+    fontFamily: "light",
+    backgroundColor: "transparent",
+    opacity: 0.54
+  },
+  selectedCategoryText: {
+    opacity: 1
+  },
+  titleText: {
+    color: "white",
+    fontSize: 30,
+    fontFamily: "regular"
+  },
+  helpContainer: {
+    height: 64,
+    alignItems: "center",
+    justifyContent: "center"
   }
 });
 
