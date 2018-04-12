9b125a766d47b1f8d755056e8b411a5a2508c3ad
Sunim Acharya <sunim.54@gmail.com>
2018-04-12 21:29:04 +0545

    Update MainApplication

--- android/app/src/main/java/com/nazar/MainApplication.java
+++ android/app/src/main/java/com/nazar/MainApplication.java
@@ -16,8 +16,17 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
+import com.reactlibrary.RNOpenCvLibraryPackage;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.LoaderCallbackInterface;
+
 import java.util.Arrays;
 import java.util.List;
+
+import org.opencv.android.OpenCVLoader;
+
+import android.util.Log;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -37,7 +46,8 @@ public class MainApplication extends Application implements ReactApplication {
             new RNSharePackage(),
             new ImagePickerPackage(),
             new RNFSPackage(),
-            new RNAdMobPackage()
+            new RNAdMobPackage(),
+          new RNOpenCvLibraryPackage()
             //new RNCameraKitPackage()
       );
     }
@@ -53,9 +63,41 @@ public class MainApplication extends Application implements ReactApplication {
     return mReactNativeHost;
   }
 
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+    @Override
+    public void onManagerConnected(int status) {
+      switch (status) {
+        case LoaderCallbackInterface.SUCCESS:
+        {
+          Log.i("OpenCV", "OpenCV loaded successfully");
+        } break;
+        default:
+        {
+          super.onManagerConnected(status);
+        } break;
+      }
+    }
+  };
+
   @Override
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
+
+    if (!OpenCVLoader.initDebug()) {
+      Log.d("OpenCv", "Error while init");
+    }
   }
+
+  public void onResume()
+  {
+    if (!OpenCVLoader.initDebug()) {
+      Log.d("OpenCV", "Internal OpenCV library not found. Using OpenCV Manager for initialization");
+      OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, mLoaderCallback);
+    } else {
+      Log.d("OpenCV", "OpenCV library found inside package. Using it!");
+      mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+    }
+  }
+
 }
