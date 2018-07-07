837ce76482858d6b42427acf13374d78a2146005
Sunim Acharya <sunim.54@gmail.com>
2018-07-07 17:51:50 +0545

    Add react-native-tensorflow

--- android/app/src/main/java/com/speks/nazar/MainApplication.java
+++ android/app/src/main/java/com/speks/nazar/MainApplication.java
@@ -3,6 +3,7 @@ package com.speks.nazar;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.rntensorflow.RNTensorFlowPackage;
 //import com.rntensorflow.RNTensorFlowPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -31,6 +32,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNTensorFlowPackage(),
             //new RNTensorFlowPackage(),
             new SplashScreenReactPackage(),
             new VectorIconsPackage(),
